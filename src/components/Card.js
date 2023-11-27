0d3572592e3891d0589cf630ee5fa733cfde306d
KSN <kapilsinghnegi@outlook.com>
2023-11-27 21:46:31 +0530

    initial commit
    added files

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Card = ( { name, email, id}) => {
+	return (
+		<div className='bg-light-green dib tc br3 pa3 ma2 grow bw-2 shadow-5'>
+			<img alt='robots' src={`https://robohash.org/${id}?set=set2`} />
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>
+	);
+}
+export default Card;
